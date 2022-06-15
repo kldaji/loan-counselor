92e1ff7c3fa88a58ce3895d5b51f7b2fc58909e2
KLDAJI <kldaji@naver.com>
2022-06-15 12:59:12 +0900

    #7 feat: add navigation graph and navigation host
    navigation graph: contains all navigation-related information.
    navigation host: empty container which displays destination from navigation graph.
    
    ref: https://developer.android.com/guide/navigation/navigation-getting-started

---
+++ app/src/main/java/com/kldaji/loancounselor/ClientsFragment.kt
@@ -0,0 +1,18 @@
+package com.kldaji.loancounselor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ClientsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_clients, container, false)
+    }
+}
