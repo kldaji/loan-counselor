7d3f32a8a1e865b91dfb21328f0c0f90557912fd
KLDAJI <kldaji@naver.com>
2022-06-16 18:21:00 +0900

    #9 feat: move items of ScheduledClientViewAdapter to ViewModel
    - ViewModel manages all data
    - The ViewModelStoreOwner is MainActivity. Thus, fragments with MainActivity host can share data through ViewModel.
    ref: https://developer.android.com/topic/libraries/architecture/viewmodel?hl=ko#sharing

--- app/src/main/java/com/kldaji/loancounselor/ScheduledClientViewAdapter.kt
+++ app/src/main/java/com/kldaji/loancounselor/ScheduledClientViewAdapter.kt
@@ -8,14 +8,11 @@ import com.kldaji.loancounselor.databinding.ItemScheduledClientBinding
 
 class ScheduledClientViewAdapter(private val itemClickCallback: ItemClickCallback) :
     RecyclerView.Adapter<ScheduledClientViewAdapter.ScheduledClientViewHolder>() {
-    private val items = mutableListOf(
-        ScheduledClientView("미팅 예정 고객",
-            "오늘의 미팅 예정 고객 명단 입니다!",
-            R.drawable.ic_meeting_24),
-        ScheduledClientView("대출실행 예정 고객",
-            "30일내 대출실행 예정 고객 명단 입니다!",
-            R.drawable.ic_action_24)
-    )
+    private lateinit var items: List<ScheduledClientView>
+
+    fun setItems(_items: List<ScheduledClientView>) {
+        items = _items
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScheduledClientViewHolder {
         val binding = DataBindingUtil.inflate<ItemScheduledClientBinding>(
