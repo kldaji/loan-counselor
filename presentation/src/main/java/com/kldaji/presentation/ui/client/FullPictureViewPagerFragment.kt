40b13e8de24e021ee518730943f5d54966ff6751
KLDAJI <kldaji@naver.com>
2022-06-24 19:14:52 +0900

    #36 feat: add TabLayout to indicate current page of ViewPager

--- presentation/src/main/java/com/kldaji/presentation/ui/client/FullPictureViewPagerFragment.kt
+++ presentation/src/main/java/com/kldaji/presentation/ui/client/FullPictureViewPagerFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.tabs.TabLayoutMediator
 import com.kldaji.presentation.databinding.FragmentFullPictureViewPagerBinding
 import com.kldaji.presentation.ui.ClientsViewModel
 import com.kldaji.presentation.ui.client.adapter.FullPictureAdapter
@@ -34,6 +35,8 @@ class FullPictureViewPagerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        TabLayoutMediator(binding.tlFullPicture, binding.vpFullPicture) { tab, position -> }
+            .attach()
     }
 
 
