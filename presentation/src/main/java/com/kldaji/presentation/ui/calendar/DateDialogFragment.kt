268d06eb6a5e109fff0dc0512327b2cc18564aa0
KLDAJI <kldaji@naver.com>
2022-07-05 22:31:26 +0900

    #27 feat: show side preview pages in ViewPager using padding
    ref: https://stackoverflow.com/questions/24634830/margin-of-a-fragments-root-view-not-registered-when-used-in-viewpager

--- presentation/src/main/java/com/kldaji/presentation/ui/calendar/DateDialogFragment.kt
+++ presentation/src/main/java/com/kldaji/presentation/ui/calendar/DateDialogFragment.kt
@@ -1,10 +1,15 @@
 package com.kldaji.presentation.ui.calendar
 
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
+import androidx.viewpager2.widget.MarginPageTransformer
 import com.kldaji.presentation.R
 import com.kldaji.presentation.databinding.DialogFragmentDateBinding
 import com.kldaji.presentation.ui.calendar.adapter.DateAdapter
@@ -24,8 +29,6 @@ class DateDialogFragment : DialogFragment() {
     private var _binding: DialogFragmentDateBinding? = null
     private val binding: DialogFragmentDateBinding get() = _binding!!
     private var timestamp = 0L
-
-    override fun getTheme(): Int = R.style.FullWidthDialog
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -48,8 +51,25 @@ class DateDialogFragment : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setDialogLayout()
+        setViewPagerPageTransformer()
+    }
+
+    private fun setDialogLayout() {
+        val windowManager =
+            requireContext().getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val rect = windowManager.currentWindowMetrics.bounds
+        println(rect.width())
+        println(rect.height())
         val height = resources.getDimensionPixelSize(R.dimen.date_dialog_height)
-        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, height)
+        dialog?.window?.setLayout(rect.width(), height)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
+
+    private fun setViewPagerPageTransformer() {
+        val pageMargin = resources.getDimensionPixelOffset(R.dimen.page_margin)
+        binding.vpDialogFragmentDate.setPageTransformer(MarginPageTransformer(pageMargin))
+        binding.vpDialogFragmentDate.offscreenPageLimit = 2
     }
 
     override fun onDestroyView() {
