9542d1960119e7145434794b20bd36a709ce1d64
KLDAJI <kldaji@naver.com>
2022-07-29 15:36:00 +0900

    [#57] feat : 파일명 변경

--- presentation/src/main/java/com/kldaji/presentation/ui/client/PhotoViewPagerFragment.kt
+++ presentation/src/main/java/com/kldaji/presentation/ui/client/PhotoViewPagerFragment.kt
@@ -6,29 +6,28 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.tabs.TabLayoutMediator
-import com.kldaji.presentation.databinding.FragmentFullPictureViewPagerBinding
+import com.kldaji.presentation.databinding.FragmentPhotoViewPagerBinding
 import com.kldaji.presentation.ui.ClientsViewModel
-import com.kldaji.presentation.ui.client.adapter.FullPictureAdapter
+import com.kldaji.presentation.ui.client.adapter.PhotoViewAdapter
 import com.kldaji.presentation.ui.client.entity.PictureItemView
 
-class FullPictureViewPagerFragment : Fragment() {
-    private var _binding: FragmentFullPictureViewPagerBinding? = null
+class PhotoViewPagerFragment : Fragment() {
+    private var _binding: FragmentPhotoViewPagerBinding? = null
     private val binding get() = _binding!!
-    private val navArgs: FullPictureViewPagerFragmentArgs by navArgs()
+    private val navArgs: PhotoViewPagerFragmentArgs by navArgs()
     private val viewModel by activityViewModels<ClientsViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        _binding = FragmentFullPictureViewPagerBinding.inflate(inflater, container, false)
+        _binding = FragmentPhotoViewPagerBinding.inflate(inflater, container, false)
         val pictureUris = viewModel.pictures.value?.map {
             PictureItemView.PictureItem(it, it)
         } ?: listOf()
-        val adapter = FullPictureAdapter(pictureUris)
+        val adapter = PhotoViewAdapter(pictureUris)
         binding.vpFullPicture.adapter = adapter
         return binding.root
     }
