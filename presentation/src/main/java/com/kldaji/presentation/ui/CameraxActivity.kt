aee49253ae29532e4b6932da27ec8e27700a33ce
KLDAJI <kldaji@naver.com>
2022-07-29 09:23:31 +0900

    [#55] feat : camera permission

--- presentation/src/main/java/com/kldaji/presentation/ui/CameraxActivity.kt
+++ presentation/src/main/java/com/kldaji/presentation/ui/CameraxActivity.kt
@@ -1,7 +1,13 @@
 package com.kldaji.presentation.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.kldaji.presentation.databinding.ActivityCameraxBinding
 
 class CameraxActivity : AppCompatActivity() {
@@ -11,5 +17,48 @@ class CameraxActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraxBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            // TODO: start camera
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults:
+        IntArray,
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                // TODO: start camera
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    companion object {
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf(
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
     }
 }
