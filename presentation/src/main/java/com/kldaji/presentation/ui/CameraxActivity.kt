67ef7217a02403ab3597a5557fadc9e9f04d8e7d
KLDAJI <kldaji@naver.com>
2022-07-29 09:28:38 +0900

    [#55] feat : start camera

--- presentation/src/main/java/com/kldaji/presentation/ui/CameraxActivity.kt
+++ presentation/src/main/java/com/kldaji/presentation/ui/CameraxActivity.kt
@@ -6,13 +6,19 @@ import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.kldaji.presentation.databinding.ActivityCameraxBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraxActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCameraxBinding
 
+    private lateinit var cameraExecutor: ExecutorService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraxBinding.inflate(layoutInflater)
@@ -20,14 +26,46 @@ class CameraxActivity : AppCompatActivity() {
 
         // Request camera permissions
         if (allPermissionsGranted()) {
-            // TODO: start camera
+            startCamera()
         } else {
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
+        cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.preview.surfaceProvider)
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview)
+
+            } catch (exc: Exception) {
+                Toast.makeText(this, "Use case binding failed", Toast.LENGTH_SHORT).show()
+            }
+        }, ContextCompat.getMainExecutor(this))
     }
 
     override fun onRequestPermissionsResult(
@@ -38,7 +76,7 @@ class CameraxActivity : AppCompatActivity() {
     ) {
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (allPermissionsGranted()) {
-                // TODO: start camera
+                startCamera()
             } else {
                 Toast.makeText(this,
                     "Permissions not granted by the user.",
@@ -46,6 +84,11 @@ class CameraxActivity : AppCompatActivity() {
                 finish()
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 
     companion object {
