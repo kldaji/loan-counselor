c1fbfc615dc38f8eb44c5636a82e7be79a5b50e8
KLDAJI <kldaji@naver.com>
2022-09-01 21:17:37 +0900

    Compose 프로젝트 생성

---
+++ compose/app/src/main/java/com/kldaji/loancounseolor/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.kldaji.loancounseolor
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kldaji.loancounseolor.ui.theme.LoanCounseolorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoanCounseolorTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LoanCounseolorTheme {
+        Greeting("Android")
+    }
+}
