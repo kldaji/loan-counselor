ff713ec1b8bf143b60d3a108ab3043185191993a
KLDAJI <kldaji@naver.com>
2022-09-28 23:02:52 +0900

    카메라, 외부저장소 Permission 체크

--- view/presentation/src/main/java/com/kldaji/presentation/ui/MainActivity.kt
+++ view/presentation/src/main/java/com/kldaji/presentation/ui/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.kldaji.presentation.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.kakao.sdk.user.UserApiClient
 import com.kldaji.presentation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     companion object {
         private const val TAG = "MainActivity"
     }
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+    private var isCameraPermissionGranted = false
+    private var isReadPermissionGranted = false
+    private var isWritePermissionGranted = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +32,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         printKakaoUserInfo()
+        setLauncher()
+        requestPermission()
+    }
+
+    private fun setLauncher() {
+        permissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                isCameraPermissionGranted =
+                    permissions[Manifest.permission.CAMERA] ?: isCameraPermissionGranted
+                isReadPermissionGranted = permissions[Manifest.permission.READ_EXTERNAL_STORAGE]
+                    ?: isReadPermissionGranted
+                isWritePermissionGranted = permissions[Manifest.permission.WRITE_EXTERNAL_STORAGE]
+                    ?: isWritePermissionGranted
+
+                if (!isCameraPermissionGranted || !isReadPermissionGranted || !isWritePermissionGranted) {
+                    // TODO: 권한 설정 화면으로 이동
+                    finish()
+                }
+            }
+    }
+
+    private fun requestPermission() {
+        isCameraPermissionGranted = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+        isReadPermissionGranted = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+        isWritePermissionGranted = (ContextCompat.checkSelfPermission(this,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+
+        val permissions = mutableListOf<String>()
+
+        if (!isCameraPermissionGranted) permissions.add(Manifest.permission.CAMERA)
+        if (!isReadPermissionGranted) permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+        if (!isWritePermissionGranted) permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        if (permissions.isNotEmpty()) permissionLauncher.launch(permissions.toTypedArray())
     }
 
     private fun printKakaoUserInfo() {
