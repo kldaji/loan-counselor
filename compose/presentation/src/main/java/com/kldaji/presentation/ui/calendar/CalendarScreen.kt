8b31551d10630c37846ea1537b8be8f35fc4e035
KLDAJI <kldaji@naver.com>
2022-10-14 23:30:48 +0900

    Compose - 캘린더 화면 툴바

--- compose/presentation/src/main/java/com/kldaji/presentation/ui/calendar/CalendarScreen.kt
+++ compose/presentation/src/main/java/com/kldaji/presentation/ui/calendar/CalendarScreen.kt
@@ -1,7 +1,8 @@
 package com.kldaji.presentation.ui.calendar
 
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
@@ -9,18 +10,35 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
 import com.kldaji.presentation.ui.theme.black
 import com.kldaji.presentation.ui.theme.blue
+import com.kldaji.presentation.ui.theme.green
 import com.kldaji.presentation.ui.theme.red
+import com.kldaji.presentation.ui.theme.yellow
 import com.kldaji.presentation.util.CalendarLogic
 import java.util.*
 
@@ -41,17 +59,73 @@ fun CalendarScreen(
 ) {
     val startPosition = Int.MAX_VALUE / 2
     val state = rememberPagerState(initialPage = startPosition)
+    var month by remember { mutableStateOf("${CalendarLogic.getMonth(0)}월") }
 
-    Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
+    Scaffold(
+        topBar = {
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 4.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "뒤로가기")
+                }
+                Text(
+                    text = month,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 22.sp
+                )
+                Column(
+                    modifier = modifier.padding(end = 16.dp),
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Box(
+                            modifier = modifier
+                                .size(8.dp)
+                                .clip(CircleShape)
+                                .background(yellow)
+                        )
+                        Text(
+                            modifier = modifier.padding(start = 8.dp),
+                            text = "미팅",
+                            fontSize = 12.sp
+                        )
+                    }
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Box(
+                            modifier = modifier
+                                .size(8.dp)
+                                .clip(CircleShape)
+                                .background(green)
+                        )
+                        Text(
+                            modifier = modifier.padding(start = 8.dp),
+                            text = "대출실행",
+                            fontSize = 12.sp
+                        )
+                    }
+                }
+            }
+        }
+    ) { paddingValues ->
         HorizontalPager(
-            modifier = modifier.fillMaxSize(),
+            modifier = modifier
+                .fillMaxSize()
+                .padding(paddingValues),
             count = Int.MAX_VALUE,
             state = state
         ) { page: Int ->
+
+            month = "${CalendarLogic.getMonth(currentPage - startPosition)}월"
+
             Column(modifier = modifier.fillMaxSize()) {
                 Days(modifier = modifier)
 
@@ -103,8 +177,7 @@ fun Month(
             Row(
                 modifier = modifier
                     .fillMaxWidth()
-                    .weight(1f)
-                    .border(width = 1.dp, color = red),
+                    .weight(1f),
                 horizontalArrangement = Arrangement.SpaceEvenly
             ) {
                 week.forEach { date ->
@@ -128,8 +201,7 @@ fun ColumnScope.Date(
     Column(
         modifier = modifier
             .fillMaxHeight()
-            .weight(1f)
-            .border(width = 1.dp, color = black),
+            .weight(1f),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
